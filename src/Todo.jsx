9a874bd13f0dc1acf4b8e8193caea9e9ae5527f0
sourav singh <ss227737@gmail.com>
2023-03-02 00:31:50 +0530

    updated

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,22 +6,48 @@ const Todo = () => {
   
   const [search, setSearch] = useState('');
   const [list , setList] = useState ([]);
+  const [toggle , setToggle] =useState(true);
+  const [editId, setEditId] = useState(null);
 
   const addItems =() => {
     if(!search){
-
+      alert(`pls enter the value`);
+    }else if (search && !toggle) {
+      setList(
+        list.map((elem)=>{
+        if(elem.id === editId){
+          return{...elem, name:search}
+        }
+        return elem;
+    })
+    
+    );
+    setToggle(true);
+    setSearch('');
+    setEditId (null);
     }else{
-      setList([...list,search]);
+      const allSearch = {id:new Date().getTime().toString(), name:search}
+      setList([...list,allSearch]);
       setSearch('');
     }
     
   }
 
   const deleteItems = (id) => {
-    const updatedItems = list.filter((curE, ind)=>{
-        return ind !== id ;
+
+    const updatedItems = list.filter((curE)=>{
+        return curE.id !== id ;
     })
     setList(updatedItems);
+  }
+
+  const updateItems = (id) => {
+    const editItems = list.find((element)=>{
+        return element.id === id    
+    });
+    setToggle(false);
+    setSearch(editItems.name);
+    setEditId (id);
   }
 
   const removeAll = () => {
@@ -44,15 +70,21 @@ const Todo = () => {
                 onChange={(e) => {setSearch(e.target.value)}}
 
                 />
-                <i className="fa fa-plus" title='Add Items' onClick={addItems}></i>
+                {
+                  toggle ? 
+                  <i className="fa fa-plus" title='Add Items' onClick={addItems}></i> :
+                  <i className="fa fa-edit" title='Update Items' onClick={addItems}></i>
+                }
+                
             </div>
              <div className="showItems">
-                { list.map((element,ind)=>{
+                { list.map((element)=>{
                   return(
-                    <div className="eachItem" key={ind}>
-                    <h2>{element}</h2>
+                    <div className="eachItem" key={element.id}>
+                    <h2>{element.name}</h2>
                     <div className="todo-btn">
-                    <i className="fas fa-trash-alt" title='Delete Items' onClick={() =>{deleteItems(ind);}}></i>
+                    <i className="fas fa-edit" title='Update Items' onClick={() =>{updateItems(element.id);}} ></i>
+                    <i className="fas fa-trash-alt" title='Delete Items' onClick={() =>{deleteItems(element.id);}}></i>
                     </div>
                     </div>
                   )
